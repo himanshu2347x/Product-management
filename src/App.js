942cdb4e59920afc53512bee3aa6ea437df77fce
himanshu2347x <himanshu.sinha392@gmail.com>
2025-01-19 14:16:45 +0530

    fixed routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <div className="App">
 
       <Routes>
+        <Route path="/" element={<Login/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/login" element={<Login/>} />
 
